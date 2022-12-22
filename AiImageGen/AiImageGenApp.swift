acb4a5a78986de443b35061a5a226f4fd0811f7f
paxmiler <paxmiler@gmail.com>
2022-12-22 18:18:06 +0900

    Initial Commit

---
+++ AiImageGen/AiImageGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiImageGenApp.swift
+//  AiImageGen
+//
+//  Created by Martin.Q on 2022/12/22.
+//
+
+import SwiftUI
+
+@main
+struct AiImageGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
