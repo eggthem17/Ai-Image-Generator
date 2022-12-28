babdf6d829e267cd363b2ff7fc3ac7c01ef57838
paxmiler <paxmiler@gmail.com>
2022-12-28 15:20:12 +0900

    + AiImageGen v0.1

--- AiImageGen/ContentView.swift
+++ AiImageGen/ContentView.swift
@@ -5,17 +5,134 @@
 //  Created by Martin.Q on 2022/12/22.
 //
 
+import OpenAIKit
 import SwiftUI
 
+final class ViewModel: ObservableObject {
+	private var openai: OpenAI?
+	
+	func setup() {
+		openai = OpenAI(Configuration(organization: "organization", apiKey: "apiKey"))
+	}
+	
+	func generateImage(prompt: String) async -> UIImage? {
+		guard let openai = openai else {
+			return nil
+		}
+		
+		do {
+			let params = ImageParameters(prompt: prompt, resolution: .medium, responseFormat: .base64Json)
+			let result = try await openai.createImage(parameters: params)
+			let data = result.data[0].image
+			let image = try openai.decodeBase64Image(data)
+			
+			return image
+		} catch {
+			print(String(describing: error))
+			return nil
+		}
+	}
+}
+
 struct ContentView: View {
+	@ObservedObject var viewModel = ViewModel()
+	@State var text = ""
+	@State var image: UIImage?
+	@State var alertState = false
+	@State var fetchingState = false
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+		NavigationView {
+			VStack {
+				Spacer()
+				
+				if let image = image {
+					Image(uiImage: image)
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+						.frame(width: 300, height: 300)
+				} else {
+					Text("Type prompt to generatre image!")
+				}
+				
+				Spacer()
+				
+				TextField("Type prompt here...", text: $text)
+					.padding()
+				
+				Button("Generate!") {
+					if !text.trimmingCharacters(in: .whitespaces).isEmpty {
+						Task {
+							fetchingState.toggle()
+							
+							let result = await viewModel.generateImage(prompt: text)
+							if result == nil {
+								print("Failed to get image")
+							}
+							self.image = result
+							
+							fetchingState.toggle()
+						}
+					}
+				}
+				.disabled(fetchingState)
+			}
+			.navigationTitle("Image Generator")
+			.onAppear{
+				viewModel.setup()
+			}
+			.padding()
+			.toolbar {
+				ToolbarItem(placement: .navigationBarLeading) {
+					NavigationLink {
+						InfoView()
+					} label: {
+						Image(systemName: "info.circle")
+					}
+
+
+				}
+				ToolbarItem(placement: .navigationBarTrailing) {
+					Button {
+						if let image = image {
+							alertState.toggle()
+						}
+					} label: {
+						Image(systemName: "square.and.arrow.up")
+					}
+					.disabled(fetchingState)
+				}
+			}
+			.alert("Alert", isPresented: $alertState) {
+				Button {
+					UIImageWriteToSavedPhotosAlbum(image!, nil, nil, nil)
+				} label: {
+					Text("Save")
+				}
+				
+				Button(role: .cancel) {
+					
+				} label: {
+					Text("Cancel")
+				}
+			} message: {
+				Text("Save this photo to your album.")
+			}
+		}
+		.overlay(content: {
+			if fetchingState {
+				ZStack(alignment: .center) {
+					Rectangle()
+						.background(.black)
+						.opacity(0.8)
+						.frame(width: 100, height: 100)
+						.cornerRadius(15)
+					
+					ProgressView()
+						.tint(.black)
+				}
+			}
+		})
     }
 }
 
---
+++ AiImageGen/InfoView.swift
@@ -0,0 +1,69 @@
+//
+//  InfoView.swift
+//  AiImageGen
+//
+//  Created by Martin.Q on 2022/12/23.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+	let blogAddr = URL(string: "https://chillog.page")
+	let openAiAddr = URL(string: "https://openai.com")
+	
+    var body: some View {
+		VStack {
+			HStack {
+				VStack(alignment: .leading) {
+					Text("Made by")
+						.font(.largeTitle)
+					
+					Link("@Eggthem17", destination: blogAddr!)
+						.tint(.orange)
+				}
+				
+				Spacer()
+				
+				Image("chillog_logo")
+					.resizable()
+					.frame(width: 60, height: 60)
+					.cornerRadius(15)
+			}
+			.frame(width: 240)
+			.padding()
+			
+			Divider()
+				.frame(minHeight: 2)
+				.background(.white)
+			
+			HStack {
+				VStack(alignment: .leading) {
+					Text("Powered by")
+						.font(.largeTitle)
+					
+					Link("@OpenAI", destination: openAiAddr!)
+				}
+				
+				Spacer()
+				
+				Image("openai_logo")
+					.resizable()
+					.frame(width: 60, height: 60)
+					.overlay(content: {
+						RoundedRectangle(cornerRadius: 15)
+							.stroke(.white, lineWidth: 3)
+					})
+			}
+			.frame(width: 240)
+			.padding()
+		}
+		.frame(width: 240)
+		.navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
